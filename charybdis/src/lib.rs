ea46decdc0be9aeb663de6ace57a81aee92a811d
Taylor Beever <taybeever@gmail.com>
2024-12-14 08:54:14 -0700

    feat: Re-export entire scylla dependency

--- charybdis/src/lib.rs
+++ charybdis/src/lib.rs
@@ -32,6 +32,7 @@ pub mod scylla {
     pub use scylla::serialize::value::SerializeValue;
     pub use scylla::statement::PagingState;
     pub use scylla::transport::iterator::TypedRowStream;
+    pub use scylla::*;
 }
 
 pub mod options {
